f4a383a5138c63748d50a7b98ceb8d369c20b74c
Tajinder Chumber <chumbert94@gmail.com>
2017-11-26 16:35:25 +0000

    ordering comments by votescore in post details

--- frontend/src/components/CommentsList.js
+++ frontend/src/components/CommentsList.js
@@ -4,7 +4,7 @@ import CommentItem from './CommentItem'
 
 class CommentsList extends Component {
   render() {
-    const { comments } = this.props
+    const comments = this.props.comments.sort((a, b) => a.voteScore < b.voteScore)
 
     return (
       <div className="jumbotron comments-list">
